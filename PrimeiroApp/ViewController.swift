97e29823874bda465cdde5b54d275e26e41f2282
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-28 17:41:06 -0300

    projeto finalizado

--- PrimeiroApp/ViewController.swift
+++ PrimeiroApp/ViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultadoLegenda: UILabel!
+    
+    @IBOutlet weak var precoAlcoolCampo: UITextField!
+    @IBOutlet weak var precoGasolinaCampo: UITextField!
+    
+    @IBAction func calcularCombustivel(_ sender: Any) {
+        
+        if let precoAlcool = precoAlcoolCampo.text {
+            if let precoGasolina = precoGasolinaCampo.text {
+                
+              let validaCampos = self.validarCampos(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                
+                if validaCampos{
+                    self.calcularMelhorPreco(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                } else {
+                    resultadoLegenda.text = "Digite os preços para calcular"
+                }
+            }
+        }
+    }
+    
+    func calcularMelhorPreco(precoAlcool: String, precoGasolina:String) {
+        if let valorAlcool = Double(precoAlcool) {
+            if let valorGasolina = Double(precoGasolina) {
+                let resultadoPreco = valorAlcool / valorGasolina
+                if resultadoPreco >= 0.7 {
+                    self.resultadoLegenda.text = "Melhor utilizar gasolina!"
+                } else {
+                    self.resultadoLegenda.text = "Melhor utilizar alcool!"
+                }
+            }
+        }
+    }
+    
+    func validarCampos(precoAlcool: String, precoGasolina:String) -> Bool {
+        var camposValidados = true
+        
+        if (precoAlcool.isEmpty) {
+            camposValidados = false
+        } else if precoGasolina.isEmpty {
+            camposValidados = false
+        }
+        
+        return camposValidados
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
