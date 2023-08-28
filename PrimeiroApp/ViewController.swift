174dc34a48915f3951e2e40f366d2bcb2583e924
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-28 16:41:40 -0300

    Initial Commit

---
+++ PrimeiroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Joao Luiz Marcelino Ribeiro on 28/08/23.
+//  Copyright © 2023 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
